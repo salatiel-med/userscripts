2a537301aaa9708fafd63638ca5c85729c1979f5
salatiel-med <salatiel.fraga@rmedcursosmedicos.com.br>
2025-08-15 13:21:04 -0300

    0.0.1

--- MGEWeb_plus.user.js
+++ MGEWeb_plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MGEWeb+
 // @namespace    https://mgeweb.medgrupo.com.br/
-// @version      2025-08-11
+// @version      0.0.1
 // @description  Melhorias para o MGEWeb
 // @author       Salatiel
 // @match        https://mgeweb.medgrupo.com.br/*
@@ -10,34 +10,38 @@
 // @run-at       document-end
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-waitForElm("mge-player-video video").then((elem)=>{
-    document.querySelector(".video-player-wrapper").appendChild(document.querySelector("mge-player-video video"))
-    document.querySelector("mge-player-video video").setAttribute("controls", "")
-    document.querySelector("mge-player-video video").style.width="100%"
+  waitForElm("mge-player-video video").then((elem) => {
+    document
+      .querySelector(".video-player-wrapper")
+      .appendChild(document.querySelector("mge-player-video video"));
+    document
+      .querySelector("mge-player-video video")
+      .setAttribute("controls", "");
+    document.querySelector("mge-player-video video").style.width = "100%";
     document.querySelector("mge-player-video video").playbackRate = 2.0;
-});
+  });
 
-function waitForElm(selector) {
-    return new Promise(resolve => {
+  function waitForElm(selector) {
+    return new Promise((resolve) => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver((mutations) => {
         if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
+          observer.disconnect();
+          resolve(document.querySelector(selector));
         }
+      });
 
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                observer.disconnect();
-                resolve(document.querySelector(selector));
-            }
-        });
-
-        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
+      // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+      });
     });
-}
+  }
 })();
